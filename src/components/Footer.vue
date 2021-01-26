3dcca04cca48a21892bcf39ea5e4f0f55b1fba5e
Maksim <maks9.1999@o2.pl>
2021-01-26 11:49:19 +0100

    beginning

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="footer">
+    <div class="name">Soccerstat.pl</div>
+    <div class="">
+      Created by Maksim Brzezinski & Kacper Buczko & Miłosz Orliński & Dawid Bronszkiewicz
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style scoped>
+    #footer
+    {
+      color: white;
+      position: absolute;
+      width: 100%;
+      height: 100px;
+      background-color: #343434;
+      bottom: 0;
+    }
+</style>
---
+++ src/components/MatchList.vue
@@ -0,0 +1,104 @@
+<template>
+    <div id="matchs">
+        <div class="wider-list">
+            <div class="list">
+                <a href="#">
+                    <div v-for="m in mecze" v-bind:key="m.id" class="match">
+                        <img v-if="m.league === 'LaLiga'" :src="require('../../hiszpania.jpeg')">
+                        <img v-else-if="m.league === 'Bundesliga'" :src="require('../../niemcy.jpeg')">
+                        <img v-else-if="m.league === 'Ligue 1'" :src="require('../../francja.jpeg')">
+                        <img v-else-if="m.league === 'Premier League'" :src="require('../../anglia.jpeg')">
+                        <img v-else-if="m.league === 'Serie A'" :src="require('../../wlochy.jpeg')">
+                        <div class="date">
+                            <span>{{ m.date }}</span>
+                        </div>
+                        <div class="info">
+                            <span>{{ m.team1 }}</span>
+                            <span>{{ m.score }}</span>
+                            <span>{{ m.team2 }}</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      mecze: [
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '5:0', date: '21.11.2020 21:00', id: 1},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 2},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 3},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 4},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 5},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 6},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 7},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 8},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 9},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 10},
+        {league: 'LaLiga', team1: 'FC Barcelona', team2: 'Real Madryt', score: '3:2', date: '23.11.2020 21:00', id: 11},
+      ]
+    }
+  }
+}
+</script>
+
+<style scope>
+    #matchs
+    {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;   
+    }
+    .wider-list
+    {
+        display: flex;
+        justify-content: center;
+        width: 80%;
+        background-color: white;
+    }
+    .list
+    {
+        width: 90%;
+        background-color: white;
+    }
+    .match:first-child
+    {
+        border-top: 1px solid #ddd;
+    }
+    .match
+    {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 25px;
+        border-bottom: 1px solid #ddd;
+        color: #777;
+        position: relative;
+    }
+    .date
+    {
+        display: inline-block;
+        width: 135px;
+        text-align: center;
+    }
+    .info
+    {
+        display: inline-block;
+        text-align: center;
+        width: 300px;
+    }
+    .info span
+    {
+        padding: 10px;
+    }
+    .list a
+    {
+        text-decoration: none;
+    }
+</style>
