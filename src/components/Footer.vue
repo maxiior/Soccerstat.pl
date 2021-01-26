b4c308718a8c6695fe06c53478c1fa5d84860914
Maksim <maks9.1999@o2.pl>
2021-01-26 20:02:20 +0100

    footer fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="footer">
-    <div class="name">Soccerstat.pl</div>
-    <div class="">
-      Created by Maksim Brzezinski & Kacper Buczko & Miłosz Orliński & Dawid Bronszkiewicz
+    <div>
+      <div class="name">Soccerstat.pl</div>
+      <div>
+        Created by Maksim Brzezinski & Kacper Buczko & Miłosz Orliński & Dawid Bronszkiewicz
+      </div>
     </div>
   </div>
 </template>
@@ -16,11 +18,20 @@ export default {
 <style scoped>
     #footer
     {
+      display: flex;
+      align-items: center;
       color: white;
       position: absolute;
       width: 100%;
       height: 100px;
       background-color: #343434;
       bottom: 0;
+      padding: 0 20px;
+      box-sizing: border-box;
+    }
+    .name
+    {
+      font-size: 20px;
+      margin-bottom: 5px;
     }
 </style>
