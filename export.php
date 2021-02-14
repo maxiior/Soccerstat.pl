6d3a5af7bd3419cfc60911fa5e6ac76a079f9a20
milorl <59122815+milorl@users.noreply.github.com>
2021-02-14 19:07:25 +0100

    export.php code update

--- export.php
+++ export.php
@@ -2,11 +2,63 @@
 
     if(isset($_POST['exportJson']))
     {
-        //kod do exportu jsona
+        require("db_connection.php");
+        $conn = $db->getConnection();
+        
+        // get Matches
+        $query = "SELECT * FROM matches";
+        if (!$result = mysqli_query($con, $query)) {
+            exit(mysqli_error($con));
+        }
+        
+        $matches = array();
+        if (mysqli_num_rows($result) > 0) 
+        {
+            while ($row = mysqli_fetch_assoc($result)) 
+            {
+                $matches[] = $row;
+            }
+        }
+        
+        $fp = fopen('matches.json', 'w');
+        fwrite($fp, json_encode($matches));
+        fclose($fp);
+        
+        mysqli_close($con);
     }
     else if(isset($_POST['exportCSV']))
     {
-        //kod do exportu csv
+        require("db_connection.php");
+        $conn = $db->getConnection();
+
+        // get Matches
+        $query = "SELECT * FROM matches";
+        if (!$result = mysqli_query($con, $query)) {
+            exit(mysqli_error($con));
+        }
+
+        $matches = array();
+        if (mysqli_num_rows($result) > 0) 
+        {
+            while ($row = mysqli_fetch_assoc($result)) 
+            {
+                $matches[] = $row;
+            }
+        }
+
+    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Disposition: attachment; filename=Matches.csv');
+    $output = fopen('php://output', 'w');
+    fputcsv($output, array('Date', 'First Team ID', 'Second Team ID', 'First Team Name', 'Second Team Name', 'Score', 'League ID', 'League Name', 'First Team Shots', 'Second Team Shots', 'First Team Possession', 'Second Team Possession', 'First Team Pass', 'Second Team Pass', 'First Team Fauls', 'Second Team Fauls'));
+
+    if (count($matches) > 0) 
+    {
+        foreach ($matches as $row) 
+        {
+            fputcsv($output, $row);
+        }
+    }
+    mysqli_close($con);
     }
 
 ?>
