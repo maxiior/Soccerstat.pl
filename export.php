726a70fa4410492b17b20d1226cf032da69d3959
maxiior <59049370+maxiior@users.noreply.github.com>
2021-02-14 09:56:04 +0100

    little addition

--- export.php
+++ export.php
@@ -1,3 +1,12 @@
 <?php
 
+    if(isset($_POST['exportJson']))
+    {
+        //kod do exportu jsona
+    }
+    else if(isset($_POST['exportCSV']))
+    {
+        //kod do exportu csv
+    }
+
 ?>
