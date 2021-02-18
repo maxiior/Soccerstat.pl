0a0f2d4f55a950f2cb1e12d393c91504e6a3e738
maxiior <59049370+maxiior@users.noreply.github.com>
2021-02-18 10:40:17 +0100

    bestteam update

--- bestteam.php
+++ bestteam.php
@@ -9,14 +9,11 @@ $twig = new \Twig\Environment($loader, [
 echo $twig->render('bestteam.twig');
 
 //TUTAJ
-$connection = @new mysqli("localhost", "root",'' , 'pilka');
+$connection = @new mysqli($host, $db_user, $db_password, $db_name);
 $sql = "SELECT MAX(ocena_w_sezonie),ROUND(AVG(Bramki_zdobyte),0),ROUND(AVG(Bramki_stracone),0),ROUND(AVG(Posiadanie_piłki),0), ROUND(AVG(Strzały),0), ROUND(AVG(Podania_celne),0), ROUND(AVG(Faule),0), name 
 FROM Ocena_zespołu_w_sezonie INNER JOIN ocena_zespołu_w_meczu ON ocena_zespołu_w_sezonie.druzyna_id = ocena_zespołu_w_meczu.team_id 
 INNER JOIN clubs ON ocena_zespołu_w_sezonie.druzyna_id = clubs.id";
 
-$result = $connection->query($sql);
-$row = $result->fetch_array(MYSQLI_BOTH);
-echo($row[2]);
 if($connection->connect_errno!=0)
 {
     echo '<script type="text/JavaScript">';
@@ -26,17 +23,29 @@ if($connection->connect_errno!=0)
 }
 else
 {
-    echo '<script type="text/javascript">';
-    echo 'vueApp.info[0].value="'.$row[7].'"';
-    echo 'vueApp.info[1].value="'.$row[1].'";';
-    echo 'vueApp.info[2].value="'.$row[2].'";';
-    echo 'vueApp.info[3].value="'.$row[3].'";';
-    echo 'vueApp.info[4].value="'.$row[4].'";';
-    echo 'vueApp.info[5].value="'.$row[5].'";';
-    echo 'vueApp.info[6].value="'.$row[6].'";';
-    echo 'vueApp.info[7].value="'.$row[0].'";';
-    echo 'vueApp.armsBest='.$arms.';';
-    echo '</script>';
+    if($result = $connection->query($sql))
+    {
+        $row = $result->fetch_array(MYSQLI_BOTH);
+        echo '<script type="text/javascript">';
+        echo 'vueApp.info[0].value="'.$row[7].'"';
+        echo 'vueApp.info[1].value="'.$row[1].'";';
+        echo 'vueApp.info[2].value="'.$row[2].'";';
+        echo 'vueApp.info[3].value="'.$row[3].'";';
+        echo 'vueApp.info[4].value="'.$row[4].'";';
+        echo 'vueApp.info[5].value="'.$row[5].'";';
+        echo 'vueApp.info[6].value="'.$row[6].'";';
+        echo 'vueApp.info[7].value="'.$row[0].'";';
+        echo 'vueApp.armsBest='.$arms.';';
+        echo '</script>';
+    }
+    else
+    {
+        echo '<script type="text/JavaScript">';
+        echo 'vueApp.showInfo=true;';
+        echo 'vueApp.infoVal="Data error.";';
+        echo '</script>';
+    }
+    
     $connection->close();
 }
 /////////////////////////////////
--- connect.php
+++ connect.php
@@ -1,8 +1,6 @@
 <?php
-
     $host = "localhost";
     $db_user = "root";
-    $db_password = "000000";
-    $db_name = "soccerstat";
-
+    $db_password = "";
+    $db_name = "pilka";
 ?>
