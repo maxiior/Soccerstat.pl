1f6e2804851d7c0b398d927fa8367280a63e8096
maxiior <59049370+maxiior@users.noreply.github.com>
2021-02-20 17:05:19 +0100

    bestteam update

--- bestteam.php
+++ bestteam.php
@@ -21,43 +21,47 @@ if($connection->connect_errno!=0)
 }
 else
 {
-	$sql3 ="select oc.ocena_w_sezonie, oc.druzyna_ID 
-	from ocena_zespołu_w_sezonie oc 
-	where ocena_w_sezonie = (select max(ocena_w_sezonie) from ocena_zespołu_w_sezonie st where oc.druzyna_ID=st.druzyna_ID)";
-	
-	$result3 = $connection->query($sql3);
-    $row3 = $result3->fetch_array(MYSQLI_BOTH);
-	
-    $sql = "SELECT ocena_w_sezonie,ROUND(AVG(Bramki_zdobyte),0),ROUND(AVG(Bramki_stracone),0),ROUND(AVG(Posiadanie_piłki),0), 
-	ROUND(AVG(Strzały),0), ROUND(AVG(Podania_celne),0), ROUND(AVG(Faule),0), name, druzyna_ID 
-	FROM Ocena_zespołu_w_sezonie INNER JOIN ocena_zespołu_w_meczu ON ocena_zespołu_w_sezonie.druzyna_id = ocena_zespołu_w_meczu.team_id 
-	INNER JOIN clubs ON ocena_zespołu_w_sezonie.druzyna_id = clubs.id WHERE druzyna_ID= \"$row3[1]\"";
+    //średnie obliczane są na podstawie mniej więcej połowy meczów, bez $sql3
 
-    if($result = $connection->query($sql))
+	$sql ="select ocena_w_sezonie, druzyna_ID from ocena_zespołu_w_sezonie order by ocena_w_sezonie DESC LIMIT 1";
+	
+	$result = $connection->query($sql);
+    $row = $result->fetch_array(MYSQLI_BOTH);
+
+    $sql2 = "SELECT ROUND(AVG(score1),0), ROUND(AVG(score2),0), ROUND(AVG(possession1),0), ROUND(AVG(shoots1),0), ROUND(AVG(apasses1),0), ROUND(AVG(fauls1),0), team1, club_id
+    FROM games WHERE club_id= \"$row[1]\"";
+
+    $sql3 = "SELECT ROUND(AVG(score2),0), ROUND(AVG(score1),0), ROUND(AVG(possession2),0), ROUND(AVG(shoots2),0), ROUND(AVG(apasses2),0), ROUND(AVG(fauls2),0)
+    FROM games WHERE club2_id= \"$row[1]\"";
+
+    if($result2 = $connection->query($sql2))
     {
-        $row = $result->fetch_array(MYSQLI_BOTH);
-		
-        $sql2 = "SELECT c.country FROM clubs AS c WHERE c.id = ".$row[8];
+        $result3 = $connection->query($sql3);
 
-        $result2 = $connection->query($sql2);
         $row2 = $result2->fetch_array(MYSQLI_BOTH);
+        $row3 = $result3->fetch_array(MYSQLI_BOTH);
+		
+        $sql4 = "SELECT c.country FROM clubs AS c WHERE c.id = ".$row2[7];
 
-        if($row2[0] == "England") { $league='Premier League'; }
-        elseif($row2[0] == "Spain") { $league='La Liga'; }
-        elseif($row2[0] == "France") { $league='Ligue 1'; }
-        elseif($row2[0] == "Germany") { $league='Bundesliga'; }
+        $result4 = $connection->query($sql4);
+        $row4 = $result4->fetch_array(MYSQLI_BOTH);
+
+        if($row4[0] == "England") { $league='Premier League'; }
+        elseif($row4[0] == "Spain") { $league='La Liga'; }
+        elseif($row4[0] == "France") { $league='Ligue 1'; }
+        elseif($row4[0] == "Germany") { $league='Bundesliga'; }
         else { $league='Serie A'; }
 
-        $path = './photos/'.$league.'/'.str_replace(" ", "_", $row[7]).'.jpg'; 
+        $path = './photos/'.$league.'/'.str_replace(" ", "_", $row2[6]).'.jpg'; 
 
         echo '<script type="text/javascript">';
-        echo 'vueApp.info[0].value="'.$row[7].'";';
-        echo 'vueApp.info[1].value="'.$row[1].'";';
-        echo 'vueApp.info[2].value="'.$row[2].'";';
-        echo 'vueApp.info[3].value="'.$row[3].'";';
-        echo 'vueApp.info[4].value="'.$row[4].'";';
-        echo 'vueApp.info[5].value="'.$row[5].'";';
-        echo 'vueApp.info[6].value="'.$row[6].'";';
+        echo 'vueApp.info[0].value="'.$row2[5].'";';
+        echo 'vueApp.info[1].value="'.$row2[0].'";';
+        echo 'vueApp.info[2].value="'.$row2[1].'";';
+        echo 'vueApp.info[3].value="'.$row2[2].'%";';
+        echo 'vueApp.info[4].value="'.$row2[3].'";';
+        echo 'vueApp.info[5].value="'.$row2[4].'";';
+        echo 'vueApp.info[6].value="'.$row2[5].'";';
         echo 'vueApp.info[7].value="'.$row[0].'";';
         echo 'vueApp.armsBest="'.$path.'";';
         echo '</script>';
